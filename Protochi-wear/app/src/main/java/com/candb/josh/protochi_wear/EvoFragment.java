cbf93805c4c179785344224f487e4aa199a08a08
josh <joshsmith2@gmail.com>
2015-11-20 18:01:27 +0000

    Added fragment (though untested) - pngs now have correct names

---
+++ Protochi-wear/app/src/main/java/com/candb/josh/protochi_wear/EvoFragment.java
@@ -0,0 +1,48 @@
+package com.candb.josh.protochi_wear;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class EvoFragment extends GenericFragment {
+
+    private View mView;
+
+    public EvoFragment() {
+        // Required empty public constructor
+    }
+
+    public static EvoFragment newInstance() {
+        return new EvoFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_evo, container, false);
+        return mView;
+    }
+
+    // Set up context menu
+    public void onResume(){
+        super.onResume();
+        registerForContextMenu(mView);
+    }
+
+    @Override
+    public void onPause()
+    {
+        unregisterForContextMenu(mView);
+        super.onPause();
+    }
+
+
+}
